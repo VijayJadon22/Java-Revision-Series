01056db5f8ddf15492fad4ebc58508e1002e2343
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-07 11:37:00 +0530

    Revised the implicit and explicit type conversion of variables

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+
+    public static void main(String[] args) {
+        float num = 5.6f;
+        char ch = 'a';
+        boolean b = true;
+        System.out.println(b);
+    }
+}
---
+++ TypeConversion.java
@@ -0,0 +1,25 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        
+        // byte b = 125;
+        // int a = 23;
+        // b = a; cannot convert from int to byte
+        // a = b;
+
+        // b = (byte) a;
+        // System.out.println(b);
+
+        // float f = 20.49f;
+        // double d = 1;
+        // d = f;
+        // System.out.println(d);
+
+        int a = 257;
+        byte b = 23;
+        b = (byte) a;
+        System.out.println(b);
+
+
+
+    }
+}
